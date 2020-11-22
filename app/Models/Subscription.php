7fcc43d29c0cc0d0d5db32a4e88cdf40c4057d3f
Loris Leiva <loris.leiva@gmail.com>
2020-11-22 12:40:38 +0000

    Add User relationship to Subscription

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -4,14 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Subscription extends Model
 {
     use HasFactory;
 
+    // Disable mass-assignment exceptions.
     protected $guarded = [];
 
+    // Cast 'cancelled_at' to a Carbon instance.
     protected $dates = ['cancelled_at'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function scopeActive($query)
     {
