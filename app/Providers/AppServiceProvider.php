89af9cd1116e2bd8ce6132481c87f92ee6fe66e5
Loris Leiva <loris.leiva@gmail.com>
2020-11-22 14:24:42 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,17 +30,22 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Kanuu::on('subscription_*', function ($payload, User $user) {
+            // Data available on all subscription events.
+            $data = [
+                'paddle_user_id' => $payload->user_id,
+                'paddle_plan_id' => $payload->subscription_plan_id,
+                'paddle_checkout_id' => $payload->checkout_id,
+                'status' => $payload->status,
+            ];
+
+            // The `cancellation_effective_date` is only available on the `subscription_cancelled` event.
+            if (isset($payload->cancellation_effective_date)) {
+                $data['cancelled_at'] = $payload->cancellation_effective_date;
+            }
+
+            // Create or update based on the paddle subscription_id.
             $user->subscriptions()->updateOrCreate(
-                [
-                    'paddle_subscription_id' => $payload->subscription_id,
-                ],
-                [
-                    'paddle_user_id' => $payload->user_id,
-                    'paddle_plan_id' => $payload->subscription_plan_id,
-                    'paddle_checkout_id' => $payload->checkout_id,
-                    'status' => $payload->status,
-                    'cancelled_at' => $payload->cancellation_effective_date ?? null,
-                ]
+                ['paddle_subscription_id' => $payload->subscription_id], $data
             );
         });
     }
